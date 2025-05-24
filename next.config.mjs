a724ed0f98ba999090b88e29bb3ab3415a98fef7
NaderGesmi <nedergesmi35@gmail.com>
2025-05-24 17:10:21 +0300

    fix: Configure static export for Netlify by updating next.config.mjs and netlify.toml

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+      },
+    ],
   },
   // Enable static exports for Netlify
   output: 'export',
   // Disable server-side features since we're using static export
   trailingSlash: true,
-  distDir: '.next',
 }
 
 export default nextConfig
