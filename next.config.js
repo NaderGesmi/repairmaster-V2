c96aab72e7a6a7939fcb5dc887252b2f805461b4
NaderGesmi <nedergesmi35@gmail.com>
2025-05-25 18:41:59 +0300

    Remove external image domains from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'placehold.co',
-      'hebbkx1anhila5yf.public.blob.vercel-storage.com'
-    ],
+    remotePatterns: []
   },
 }
 
