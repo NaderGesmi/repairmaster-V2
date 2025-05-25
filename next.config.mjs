2c67aba22f20afdfc1c6682c6663cdda0a4229a8
NaderGesmi <nedergesmi35@gmail.com>
2025-05-25 21:52:39 +0300

    chore: update next.config.mjs for Netlify compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,14 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ['res.cloudinary.com'], // allow Cloudinary domain
+    domains: ['res.cloudinary.com'], // allow Cloudinary domain for images
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/katakuri740/**',
+      }
+    ],
     unoptimized: true, // disable Next.js image optimization for Netlify compatibility
   },
 }
